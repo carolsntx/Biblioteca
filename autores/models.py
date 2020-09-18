7081b0bf2981278e514f8aae71c98153fba3f2a6
carolsntx <ann4santosf@gmail.com>
2020-09-17 23:03:48 -0300

    Adicionado model de genero e autor

--- autores/models.py
+++ autores/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from autores.models import Autor
-class Livros(models.Model):
-    codigo = models.IntegerField(primary_key=True)
+
+class Autor(models.Model):
+    codigo = models.IntegerField()
     nome = models.CharField(max_length=30)
 
     def __str__(self):
--- generos/models.py
+++ generos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from autores.models import Autor
-class Livros(models.Model):
+
+class Genero(models.Model):
     codigo = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=30)
-
+    
     def __str__(self):
         return self.nome
